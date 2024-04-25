13725c1d1dcbbba77acc49fd858009b15e999451
Damian Niemczyk-Brunnbauer <djniemczyk7@googlemail.com>
2024-04-26 00:20:54 +0200

    Create functions for a railway type operation of the functions

--- lib/fizz_buzz.ex
+++ lib/fizz_buzz.ex
@@ -13,9 +13,13 @@ defmodule FizzBuzz do
 
   """
   def parse(num \\ 3) do
-    1..num
+    fizzbuzzed_numbers = 1..num
+
+    fizzbuzzed_numbers
+    |> Enum.map(fn x -> {x, x} end)
     |> Enum.map(&fizz/1)
     |> Enum.map(&buzz/1)
+    |> Enum.map(fn x -> elem(x, 0) end)
   end
 
   def fizz(num, x) when is_number(num) do
@@ -36,7 +40,8 @@ defmodule FizzBuzz do
 
   @doc """
   Returns the string "Fizz" if the number is divisible by 3. In all
-  other cases returns the input number.
+  other cases returns the input number. If provided with a tuple
+  returns for combining the fizz function in a railway style map.
 
   ## Examples
 
@@ -45,9 +50,28 @@ defmodule FizzBuzz do
 
       iex> FizzBuzz.fizz(5)
       5
+
+      iex> FizzBuzz.fizz({3, 3})
+      {"Fizz", 3}
+
+      iex> FizzBuzz.fizz({5, 5})
+      {5, 5}
+
+      iex> FizzBuzz.fizz({"Buzz", 3})
+      {"BuzzFizz", 3}
+
+      iex> FizzBuzz.fizz({"Buzz", 5})
+      {"Buzz", 5}
   """
   def fizz(num) when is_number(num) do
     if rem(num, 3) == 0, do: "Fizz", else: num
+  end
+
+  def fizz(x) when is_tuple(x) do
+    first = elem(x, 0)
+    second = elem(x, 1)
+
+    {fizz(first, second), second}
   end
 
   def fizz(x) do
@@ -86,6 +110,14 @@ defmodule FizzBuzz do
   def buzz(num) when is_number(num) do
     if rem(num, 5) == 0, do: "Buzz", else: num
   end
+
+  def buzz(x) when is_tuple(x) do
+    first = elem(x, 0)
+    second = elem(x, 1)
+
+    {buzz(first, second), second}
+  end
+
 
   def buzz(x) do
     x
